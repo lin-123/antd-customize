8ec4c05787f71dd16b7edbc2169c7d3e50ba7f6c
wangjiaqi96 <wangjiaqi96@jd.com>
2022-09-07 14:42:44 +0800

    feat:简化文件夹生成方法mkdir

--- generator.js
+++ generator.js
@@ -1,56 +1,43 @@
 const fs = require('fs');
-const path = require('path');
 const antd = require('antd');
 const lodash = require('lodash');
 
 const componentArr = Object.keys(antd);
-
-// 递归创建目录 异步方法
-function mkdirs(dirname, callback) {
-  fs.access(dirname, fs.constants.F_OK, function (err) {
-    if (!err) {
-      callback();
-    } else {
-      console.log(path.dirname(dirname));
-      mkdirs(path.dirname(dirname), function () {
-        fs.mkdir(dirname, callback);
-        console.log(
-          '在' + path.dirname(dirname) + '目录创建好' + dirname + '目录',
-        );
-      });
-    }
-  });
-}
 
 const generate = (componentName) => {
   const content = `import { ${componentName} } from 'antd';
 import 'antd/es/${lodash.kebabCase(componentName)}/style';
 import './index.less';
 
-export default ${componentName};`;
+export default ${componentName};\r\n`;
 
   // 遍历生成代理component组件
-  mkdirs(`./src/components/${lodash.kebabCase(componentName)}`, () => {
-    // 写content，往src/${componentName}/index.js写
-    fs.writeFile(
-      `./src/components/${lodash.kebabCase(componentName)}/index.tsx`,
-      content,
-      function (err) {
-        if (err) {
-          console.log(err);
-        }
-      },
-    );
-    fs.writeFile(
-      `./src/components/${lodash.kebabCase(componentName)}/index.less`,
-      '',
-      function (err) {
-        if (err) {
-          console.log(err);
-        }
-      },
-    );
-  });
+  fs.mkdir(
+    `./src/components/${lodash.kebabCase(componentName)}`,
+    { recursive: true },
+    (err) => {
+      if (err) return false;
+      // 写content，往src/${componentName}/index.js写
+      fs.writeFile(
+        `./src/components/${lodash.kebabCase(componentName)}/index.tsx`,
+        content,
+        function (err) {
+          if (err) {
+            console.log(err);
+          }
+        },
+      );
+      fs.writeFile(
+        `./src/components/${lodash.kebabCase(componentName)}/index.less`,
+        '',
+        function (err) {
+          if (err) {
+            console.log(err);
+          }
+        },
+      );
+    },
+  );
 };
 
 let componentList = `import './style';\r\n`;
